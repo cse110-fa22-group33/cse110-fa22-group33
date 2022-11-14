f7ff19b30d16b4adeb2474e930216024584447c6
Sky Li <lyxsky@hotmail.com>
2022-11-13 16:20:50 -0800

    add destription and task_uid

--- task.js
+++ task.js
@@ -1,20 +1,20 @@
-function Task(name='New Task', uid=null, category=[], duration=1, softddl=null,
+function Task(name='New Task', uid=null, task_uid=null, category=[], duration=1, softddl=null,
               ddl=null, decription=null, mintime=1, maxtime=3, notes=null,
-              recurrent=false, busy=false, difficulty=3, start_date=null,
-              same_task=[]) {
-  this.name = name;
-  this.uid = uid;
-  this.category = category;
-  this.duration = duration;
-  this.softddl = softddl;
-  this.ddl = ddl;
-  this.mintime = mintime;
-  this.maxtime = maxtime;
-  this.notes = notes;
-  this.recurrent=recurrent;
-  this.busy=busy;
-  this.decription=decription;
-  this.difficulty=difficulty;
-  this.start_date=start_date;
-  this.same_task=same_task;
+              recurrent=false, padding=false, difficulty=3, start_date=null) {
+  this.name = name; //a string, the name of the task, not required
+  this.uid = uid; // a integer, the unique identifier of the splitted task, required (expect when task is padding)
+  this.task_uid = task_uid; // a integer, the unique identifier of the same task, all splitted task have the same task_uid, required (expect when task is padding)
+  this.category = category; // an array of string, include all categories, not required
+  this.decription=decription; // a string, destription of the task, not required
+  this.duration = duration; // a integer, the estimated duration of task, not required
+  this.softddl = softddl; // a date object, soft deadline, not required
+  this.ddl = ddl; // a date object, hard deadline, requried (expect padding)
+  this.mintime = mintime; // a integer, not required
+  this.maxtime = maxtime; // a integer, not required
+  this.notes = notes; // a string, not required
+  this.recurrent=recurrent; // a boolean, true means recurrent tasks, not required
+  this.padding=padding; // a boolean, indicate if the task is a padding (busy hour), not required
+  if (padding) {this.recurrent=true};
+  this.difficulty=difficulty; // a integer from 1-5, 1 is lowest difficulty and 5 is hardest, not required
+  this.start_date=start_date; // a date object, not required
 }
