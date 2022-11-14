0de85818cab3089cd0cb699bfc2463c33b7b16ee
Sky Li <lyxsky@hotmail.com>
2022-11-13 16:36:32 -0800

    update description

--- task.js
+++ task.js
@@ -1,3 +1,9 @@
+/**
+ * inside local storage:
+ *      a tasks object the holds an map {key=uid, value=Task object}
+ *      a current_task object that holds integer: uid
+ *      a current_week object that holds array of uid in current week
+ */
 function Task(name='New Task', uid=null, task_uid=null, category=[], duration=1, softddl=null,
               ddl=null, decription=null, mintime=1, maxtime=3, notes=null,
               recurrent=false, padding=false, difficulty=3, start_date=null) {
@@ -6,14 +12,14 @@ function Task(name='New Task', uid=null, task_uid=null, category=[], duration=1,
   this.task_uid = task_uid; // an integer, the unique identifier of the same task, all splitted task have the same task_uid, required (expect when task is padding)
   this.category = category; // an array of string, include all categories, not required
   this.decription=decription; // a string, destription of the task, not required
-  this.duration = duration; // a integer, the estimated duration of task, not required
+  this.duration = duration; // a integer, the estimated duration of task (in hour), not required
   this.softddl = softddl; // a date object, soft deadline, not required
   this.ddl = ddl; // a date object, hard deadline, requried (expect padding)
-  this.mintime = mintime; // a integer, not required
-  this.maxtime = maxtime; // a integer, not required
+  this.mintime = mintime; // a integer, in hour, not required
+  this.maxtime = maxtime; // a integer, in hour, not required
   this.notes = notes; // a string, not required
   this.recurrent=recurrent; // a boolean, true means recurrent tasks, not required
-  this.padding=padding; // a boolean, indicate if the task is a padding (busy hour), not required
+  this.padding=padding; // a boolean, indicate if the task is a padding (user-defined busy period), not required
   if (padding) {this.recurrent=true};
   this.difficulty=difficulty; // a integer from 1-5, 1 is lowest difficulty and 5 is hardest, not required
   this.start_date=start_date; // a date object, not required
