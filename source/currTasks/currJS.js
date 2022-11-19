8e2e0114f8d77201991d9e3d3b0114c3fa5ac3f2
shastasubramanian <shasta.subramanian@gmail.com>
2022-11-19 13:21:19 -0800

    updating currTask section files

--- source/currTasks/currJS.js
+++ source/currTasks/currJS.js
@@ -1,143 +1,168 @@
+import { Task } from './skyTask.js'; // put this under script.js to import this class
+
 window.addEventListener('load',(event)=>{
-    tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    const nameInput = document.querySelector('#user-name');
-    const taskInput = document.querySelector('#new-task');
-    const username = localStorage.getItem('user-name') || '';
-    const collapse = document.querySelector('.advance');
-    collapse.addEventListener('click',function(){
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-          content.style.display = "none";
-        } else {
-          content.style.display = "block";
-        }
-    });
-  
-    nameInput.value = username;
-  
-    nameInput.addEventListener('change',(event)=>{
-        localStorage.setItem('user-name', event.target.value);
-    });
-  })  
-  
-  // main.js
-  
-  // Run the init() function when the page has loaded
-  window.addEventListener('DOMContentLoaded', init);
-  
-  // Starts the program, all function calls trace back here
-  function init() {
-  // Get the recipes from localStorage
-  let tasks = getTasksFromStorage();
-  // Add each recipe to the <main> element
-  addTasksToDocument(tasks);
-  // Add the event listeners to the form elements
-  initFormHandler();
-  }
-  
-  /**
-  * Reads 'recipes' from localStorage and returns an array of
-  * all of the recipes found (parsed, not in string form). If
-  * nothing is found in localStorage for 'recipes', an empty array
-  * is returned.
-  * @returns {Array<Object>} An array of recipes found in localStorage
-  */
-  function getTasksFromStorage() {
-  // A9. TODO - Complete the functionality as described in this function
-  //           header. It is possible in only a single line, but should
-  //           be no more than a few lines.
-  
+  console.log("load");        // LOG
+  // Get tasks from local storage
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  // Query user name
+  const nameInput = document.querySelector('#user-name');
+  // Query new task
+  const taskInput = document.querySelector('#new-task');
+  const username = localStorage.getItem('user-name') || '';
+  const collapse = document.querySelector('.advance');
+  // Collapsible
+  collapse.addEventListener('click',function(){
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+  });
+
+  // Track name changes
+  nameInput.value = username;
+  nameInput.addEventListener('change',(event)=>{
+      console.log("change");        // LOG
+      localStorage.setItem('user-name', event.target.value);
+  });
+})  
+
+// Run the init() function when the page has loaded
+window.addEventListener('DOMContentLoaded', init);
+
+// Starts the program, all function calls trace back here
+function init() {
+// Get the tasks from localStorage
+let tasks = getTasksFromStorage();
+// Add each task to the <main> element
+addTasksToDocument(tasks);
+// Add the event listeners to the form elements
+initFormHandler();
+}
+
+/**
+* Reads 'tasks' from localStorage and returns an array of
+* all of the tasks found (parsed, not in string form). If
+* nothing is found in localStorage for 'tasks', an empty array
+* is returned.
+* @returns {Array<Object>} An array of tasks found in localStorage
+*/
+function getTasksFromStorage() {
+  console.log("getTasksFromStorage");        // LOG
   let r = localStorage.getItem('tasks');
-  if (!r){
+  if (!r)
     return [];
-  }
   return JSON.parse(r);
-  }
-  
-  /**
-  * Takes in an array of recipes and for each recipe creates a
-  * new <recipe-card> element, adds the recipe data to that card
-  * using element.data = {...}, and then appends that new recipe
-  * to <main>
-  * @param {Array<Object>} tasks An array of recipes
-  */
-  function addTasksToDocument(tasks) {
-  // A10. TODO - Get a reference to the <main> element
-  // A11. TODO - Loop through each of the recipes in the passed in array,
-  //            create a <recipe-card> element for each one, and populate
-  //            each <recipe-card> with that recipe data using element.data = ...
-  //            Append each element to <main>
+}
+
+/**
+* Takes in an array of tasks and for each task creates a
+* new <my-task> element, adds the task data to that card
+* using element.data = {...}, and then appends that new task
+* to <main>
+* @param {Array<Object>} tasks An array of recipes
+*/
+function addTasksToDocument(tasks) {
+  console.log("addTasksToDocument");        // LOG
+  // Loop through each of the tasks in the passed in array,
+  // create a <my-task> element for each one, and populate
+  // each <my-task> with that task data using element.data = ...
+  // Append each element to <main>
   let list = document.querySelector('#list');
   for(let t = 0; t < tasks.length; t++){
     let task = document.createElement('my-task');
     task.data = tasks[t];
     list.appendChild(task);
   }
-  
-  }
-  
-  /**
-  * Takes in an array of recipes, converts it to a string, and then
-  * saves that string to 'recipes' in localStorage
-  * @param {Array<Object>} tasks An array of recipes
-  */
-  function saveTasksToStorage(tasks) {
-  // EXPLORE - START (All explore numbers start with B)
-  // B1. TODO - Complete the functionality as described in this function
-  //            header. It is possible in only a single line, but should
-  //            be no more than a few lines.
+}
+
+/**
+* Takes in an array of tasks, converts it to a string, and then
+* saves that string to 'tasks' in localStorage
+* @param {Array<Object>} tasks An array of tasks
+*/
+function saveTasksToStorage(tasks) {
+  console.log("saveTasksToStorage");        // LOG
   let str_tasks = JSON.stringify(tasks);
   localStorage.setItem('tasks',str_tasks);
+}
+
+function assignDateAndTime(){
+  // Check if anything in storage
+    // if nothing in storage (first task inputted)
+    // set date to "tomorrow" and time to midnight
+    const today = new Date()
+    let assignedDate =  new Date()
+    assignedDate.setDate(today.getDate() + 1)
+
+    // else if something in storage (at least one task already exists)
+    // if possible to fit new task duration directly after previous task
+    // then do so
+    // else set assignedDate to the next day and time to midnight
+}
+
+// new Task('task name', 999, 2000, new Date(),['eating','studying','working']).addToLocalStorage();
+
+// let mytask = new Task('task name', 1000, 2000, new Date());
+// mytask.addToLocalStorage();
+
+
+function generateUID(){
+  const task_uid = taskName +"_"+assignedDate+"_"+assignedTime;
+}
+
+/**
+* Adds the necesarry event handlers to <form> and the clear storage
+* <button>.
+*/
+function initFormHandler() {
+let list = document.querySelector('#list');
+// Get a reference to the <form> element
+let form = document.querySelector('form');
+// Add an event listener for the 'submit' event, which fires when the
+// submit button is clicked
+form.addEventListener('submit',(event)=>{
+  event.preventDefault();
+  // Create a new FormData object from the <form> element reference above
+  let fd = new FormData(form);
+  // Create an empty taskObject, and extract the keys and corresponding
+  // values from the FormData object and insert them into taskObject
+  let taskObject = {};
+  for(const [key,val] of fd){
+    // if(!val){
+    //   continue;
+    // }
+    taskObject[key] = val;
   }
+
+  // Create a new <my-task> element
+  let new_task = document.createElement('my-task');
+  // Add the taskObject data to <my-task> using element.data
+  new_task.data = taskObject;
+  // Append this new <my-task> to <new_task>
+  list.appendChild(new_task);
+  // Get the recipes array from localStorage, add this new recipe to it, and
+  // then save the recipes array back to localStorage
+  let tasks = getTasksFromStorage();
+  tasks.push(taskObject);
+  saveTasksToStorage(tasks);
+
+  //create task object
+  let new_task_obj = new Task();
+  new_task_obj.data = taskObject;
+  console.log(new_task_obj);
   
-  /**
-  * Adds the necesarry event handlers to <form> and the clear storage
-  * <button>.
-  */
-  function initFormHandler() {
-  let list = document.querySelector('#list');
-  // B2. TODO - Get a reference to the <form> element
-  let form = document.querySelector('form');
-  // B3. TODO - Add an event listener for the 'submit' event, which fires when the
-  //            submit button is clicked
-  console.log('before submit');
-  form.addEventListener('submit',(event)=>{
-    // Steps B4-B9 will occur inside the event listener from step B3
-    console.log('try');
-    event.preventDefault();
-    // B4. TODO - Create a new FormData object from the <form> element reference above
-    let fd = new FormData(form);
-    // B5. TODO - Create an empty object (I'll refer to this object as taskObject to
-    //            make this easier to read), and then extract the keys and corresponding
-    //            values from the FormData object and insert them into taskObject
-    let taskObject = {};
-    for(const [key,val] of fd){
-      taskObject[key] = val;
-    }
-    console.log(taskObject);
-    // B6. TODO - Create a new <my-task> element
-    let new_task = document.createElement('my-task');
-    // B7. TODO - Add the taskObject data to <my-task> using element.data
-    new_task.data = taskObject;
-    // B8. TODO - Append this new <my-task> to <new_task>
-    list.appendChild(new_task);
-    // B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
-    //            then save the recipes array back to localStorage
-    let tasks = getTasksFromStorage();
-    tasks.push(taskObject);
-    saveTasksToStorage(tasks);
-  })
-  
-  // B10. TODO - Get a reference to the "Clear Local Storage" button
-  let clear_stg = document.querySelector('.danger');
-  // B11. TODO - Add a click event listener to clear local storage button
-  clear_stg.addEventListener('click',(event)=>{
-    // Steps B12 & B13 will occur inside the event listener from step B11
-    // B12. TODO - Clear the local storage
-    //event.preventDefault();
-    localStorage.clear();
-    // B13. TODO - Delete the contents of <main>
-    list.innerHTML = '';
-  })
-  }
+})
+
+// Get a reference to the "Clear Local Storage" button
+let clear_stg = document.querySelector('.danger');
+// Add a click event listener to clear local storage button
+clear_stg.addEventListener('click',(event)=>{
+  // Clear the local storage
+  localStorage.clear();
+  // Delete the contents of <main>
+  list.innerHTML = '';
+})
+}
