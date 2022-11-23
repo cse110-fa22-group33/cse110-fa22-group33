79563ea396cccde42df3692283a7ffa1af310975
Bradley Chester <68435346+bchester102@users.noreply.github.com>
2022-11-22 18:07:17 -0800

    Added 4 tests for skyTasks, still WIP

--- source/skyTasks.test.js
+++ source/skyTasks.test.js
@@ -1,5 +1,35 @@
 import { Task } from "./skyTasks";
 
 describe('Task Class Tests', () => {
+  let testTask;
+  beforeEach(() => {
+    testTask = new Task('Test Task', 1000);
+  });
 
+  test('Test Task Creation', () =>{
+    //Check if the task created in before all exists
+    expect(testTask).toBeInstanceOf(Task);
+  });
+
+  test('Convert Task to JSON string then back to task object', ()=>{
+    let jsonString = testTask.toJson();
+    expect(JSON.parse(jsonString)).toBeInstanceOf(Object);
+    expect(JSON.parse(jsonString)).toHaveProperty("task_name");
+    let taskObjectFromJSON = Task.fromJson(jsonString);
+    expect(taskObjectFromJSON).toBeInstanceOf(Task);
+    expect(taskObjectFromJSON.data.task_name).toEqual("Test Task");
+    expect(taskObjectFromJSON.data.uid).toBe(1000);
+  })
+
+  test('Check padding branch in constructor', () => {
+    let paddingTest = new Task('padding test', 1000, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, true);
+    expect(paddingTest.data.priority).toBe(6);
+    expect(paddingTest.data.padding).toBe(true);
+  })
+
+  test('Test setToPadding()', () => {
+    testTask.setToPadding();
+    expect(testTask.data.padding).toBe(true);
+    expect(testTask.data.priority).toBe(6);
+  })
 })
