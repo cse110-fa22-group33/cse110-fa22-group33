7e7f4185ab6185285b2111ba48b9cecaf7c5d9d4
Zihang-He <93348628+Zihang-He@users.noreply.github.com>
2022-11-22 17:11:20 -0800

    styles for tasks

--- source/currTasks/Task.js
+++ source/currTasks/Task.js
@@ -4,15 +4,82 @@ class Task extends HTMLElement{
         let shadowEle = this.attachShadow({mode:'open'});
         let articleEle = document.createElement('article');
         let styleEle = document.createElement('style');
+        // styleEle.textContent = `
+        //     * {
+        //         padding: 0.2rem;
+        //         padding-left: 1rem;
+        //         position: relative;
+        //     }
+        //     taskName {
+        //         font-weight:bold;
+        //     }`;
         styleEle.textContent = `
-            * {
-                padding: 0.2rem;
-                padding-left: 1rem;
-                position: relative;
-            }
-            taskName {
+        taskName {
                 font-weight:bold;
-            }`;
+        }
+        .task-card {
+            margin: 20px;
+            padding: 20px;
+            width: 200px;
+            min-height: 25px;
+            display: grid;
+            grid-template-rows: 10px 20px 0.5fr 20px;
+            border-radius: 10px;
+            box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.25);
+            transition: all 0.2s;
+            background: radial-gradient(#1fe4f5, #3fbafe);
+          }
+        
+          .task-card:hover {
+            box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.4);
+            transform: scale(1.01);
+          }
+        
+          .card__link,
+        .card__exit,
+        .card__icon {
+          position: relative;
+          text-decoration: none;
+          color: rgba(255, 255, 255, 0.9);
+        }
+        
+        .card__link::after {
+          position: absolute;
+          top: 25px;
+          left: 0;
+          content: "";
+          width: 0%;
+          height: 3px;
+          background-color: rgba(255, 255, 255, 0.6);
+          transition: all 0.5s;
+        }
+        
+        .card__link:hover::after {
+          width: 100%;
+        }
+        
+        .card__exit {
+          grid-row: 1/2;
+          justify-self: end;
+        }
+        
+        .card__icon {
+          grid-row: 2/3;
+          font-size: 30px;
+        }
+        
+        .card__title {
+          grid-row: 3/4;
+          font-weight: 400;
+          color: #ffffff;
+        }
+        
+        .card__apply {
+          grid-row: 4/5;
+          align-self: center;
+        }`;
+        
+        
         shadowEle.appendChild(styleEle);
         /*styleEle.textContent = `
             * {
@@ -82,23 +149,31 @@ class Task extends HTMLElement{
         }
 
         let article = this.shadowRoot.querySelector('article');
-        article.innerHTML = `
-        <details>
-        <summary>
-            <taskName>${data.task_name}</taskName> (${data.ddl})
-        </summary>
-        <p>Category: ${data.category}</p>
-        <p>Priority: ${data.priority}</p>
-        <p>Duration: ${data.duration} hours</p>
-        <p>Difficulty: ${data.difficulty}/5</p>
-        <p>Description: ${data.description}</p>
-        <p>DDL Time: ${data.taskddltime}</p>
-        <p>Specified DateTime: ${data.datetime}</p>
-        <p>Min Work Time: ${data.mintime}</p>
-        <p>Max Work Time: ${data.maxtime}</p>
-        <p>Extra Notes: ${data.notes}</p>
-        </details>
-        `
+        // article.innerHTML = `
+        // <details>
+        // <summary>
+        //     <taskName>${data.task_name}</taskName> (${data.ddl})
+        // </summary>
+        // <p>Category: ${data.category}</p>
+        // <p>Priority: ${data.priority}</p>
+        // <p>Duration: ${data.duration} hours</p>
+        // <p>Difficulty: ${data.difficulty}/5</p>
+        // <p>Description: ${data.description}</p>
+        // <p>DDL Time: ${data.taskddltime}</p>
+        // <p>Specified DateTime: ${data.datetime}</p>
+        // <p>Min Work Time: ${data.mintime}</p>
+        // <p>Max Work Time: ${data.maxtime}</p>
+        // <p>Extra Notes: ${data.notes}</p>
+        // </details>
+        // `
+        article.innerHTML = 
+        `<div class="task-card">
+        <p class="card__exit"><i class="fas fa-times"></i></p>
+        <card__title><taskName>${data.task_name}</taskName> (${data.ddl})<card__title>
+        <p class="card__apply">
+            Details <i class="fas fa-arrow-right">→</i>
+        </p>
+        </div>`
     }
 }
 
