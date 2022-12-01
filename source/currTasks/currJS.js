18b8e3e4c4ffc51dd829523a7589009143669998
shastasubramanian <shasta.subramanian@gmail.com>
2022-11-30 16:24:41 -0800

    comments added for adjusting task display

--- source/currTasks/currJS.js
+++ source/currTasks/currJS.js
@@ -419,6 +419,57 @@ function initFormHandler() {
     })
 }
 
+
+
+/* Adjusting Task Display Method -> NO SHADOW ELEMENT
+function DisplayTodos () {
+	const todoList = document.querySelector('#list');
+	todoList.innerHTML = "";
+
+	todos.forEach(todo => {
+		const todoItem = document.createElement('div');
+		todoItem.classList.add('todo-item');
+
+		const label = document.createElement('label');
+		const input = document.createElement('input');
+		const span = document.createElement('span');
+		const content = document.createElement('div');
+		const actions = document.createElement('div');
+		const edit = document.createElement('button');
+		const deleteButton = document.createElement('button');
+
+		input.type = 'checkbox';
+		input.checked = todo.done;
+		span.classList.add('bubble');
+		if (todo.category == 'personal') {
+			span.classList.add('personal');
+		} else {
+			span.classList.add('business');
+		}
+		content.classList.add('todo-content');
+		actions.classList.add('actions');
+		edit.classList.add('edit');
+		deleteButton.classList.add('delete');
+
+		content.innerHTML = `<input type="text" value="${todo.content}" readonly>`;
+
+		label.appendChild(input);
+		label.appendChild(span);
+		actions.appendChild(edit);
+		actions.appendChild(deleteButton);
+		todoItem.appendChild(label);
+		todoItem.appendChild(content);
+		todoItem.appendChild(actions);
+
+		todoList.appendChild(todoItem);
+
+		if (todo.done) {
+			todoItem.classList.add('done');
+		}
+		
+	})
+}*/
+
 /*
 Testing New Modal Stuff
 
