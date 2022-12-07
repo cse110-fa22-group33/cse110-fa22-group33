e21aed27069c9f8b07de6c6cdc8e2e179492c9ac
Bradley Chester <68435346+bchester102@users.noreply.github.com>
2022-12-06 19:55:30 -0800

    Weekly E2E tests

--- source/week/script.E2E.test.js
+++ source/week/script.E2E.test.js
@@ -1,30 +1,32 @@
 const path = require('path');
 
-describe('Monthly Calendar Tests', () => {
+describe('Weekly Calendar Tests', () => {
   let currentDate;
   beforeAll(async () => {
     await page.goto("http://127.0.0.1:5500/source/week/weekly.html")
+    currentDate = new Date();
   });
 
-  /*test('Test month and year header', async () => {
+  test('Test month and year header', async () => {
     let monthYear = await page.$('#monthYear');
     monthYear = await monthYear.getProperty('innerText');
     expect(await monthYear.jsonValue()).toEqual(`${currentDate.toLocaleString('default', { month: 'long' })} ${currentDate.toLocaleString('default', { year: 'numeric' })}`);
   });
 
   test('Test start and end of week days', async () => {
+    for (let index = currentDate.getDay(); index > 0; index--){
+      currentDate.setDate(currentDate.getDate() - 1);
+    }
+    let initialDate = currentDate.getDate();
+    currentDate.setDate(initialDate + 6);
+    let finalDate = currentDate.getDate();
     let weekDays = await page.$('#weekDays');
     weekDays = await weekDays.getProperty('innerText');
-    expect(await weekDays.jsonValue()).toEqual(`${currentDate.toLocaleString('default', { month: 'long' })} ${currentDate.toLocaleString('default', { year: 'numeric' })}`);
+    expect(await weekDays.jsonValue()).toEqual(`${initialDate} - ${finalDate}`);
   });
 
-  test('Test padding display', async () => {
-    const numDays = await page.$$('.day');
-    expect(numDays.length).toBe(35);
+  test('Test timeslot display', async () => {
+    const numRows = await page.$$('.grid-container');
+    expect(numRows.length).toBe(25);
   });
-
-  test('Test task display', async () => {
-    const numDays = await page.$$('.day');
-    expect(numDays.length).toBe(35);
-  });*/
 });
